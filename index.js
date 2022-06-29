0bf04579a95ccc3097aebd32cca754e2d92a4a27
ariw96 <ariw96@gmail.com>
2022-06-29 17:13:16 +0300

    more translate funcs

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const hebrewToArabicInHebrewText = async (word) => {
 	const browser = await puppeteer.launch({
 		headless: false,
 		defaultViewport: {
-			width: 1920,
-			height: 1080,
+			width: 300,
+			height: 600,
 		},
 	});
 	const page = await browser.newPage();
@@ -13,7 +13,70 @@ const hebrewToArabicInHebrewText = async (word) => {
 		const tag = document.querySelector(".result .arb.keter");
 		return tag.innerText;
 	});
-	console.log(grabWord.split("").reverse().join(""));
+	const translatedWord = grabWord.split("").reverse().join("");
+	console.log(translatedWord);
 	await browser.close();
+	return translatedWord;
 };
-hebrewToArabicInHebrewText("בית");
+const arabicToHebrew = async (word) => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		defaultViewport: {
+			width: 300,
+			height: 600,
+		},
+	});
+	const page = await browser.newPage();
+	await page.goto(`https://rothfarb.info/ronen/arabic/?searchString=${word}`);
+	const grabWord = await page.evaluate(() => {
+		const tag = document.querySelector(".result a");
+		return tag.innerText;
+	});
+	const translatedWord = grabWord.split("").reverse().join("");
+	console.log(translatedWord);
+	await browser.close();
+	return translatedWord;
+};
+const hebrewToArabic = async (word) => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		defaultViewport: {
+			width: 300,
+			height: 600,
+		},
+	});
+	const page = await browser.newPage();
+	await page.goto(`https://rothfarb.info/ronen/arabic/?searchString=${word}`);
+	const grabWord = await page.evaluate(() => {
+		const tag = document.querySelector(".result .arb.harm");
+		return tag.innerText;
+	});
+	const translatedWord = grabWord.split("").reverse().join("");
+	console.log(translatedWord);
+	await browser.close();
+	return translatedWord;
+};
+const hebrewToArabicInEnglishText = async (word) => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		defaultViewport: {
+			width: 300,
+			height: 600,
+		},
+	});
+	const page = await browser.newPage();
+	await page.goto(`https://rothfarb.info/ronen/arabic/?searchString=${word}`);
+	const grabWord = await page.evaluate(() => {
+		const tag = document.querySelector(".result .eng");
+		return tag.innerText;
+	});
+	const translatedWord = grabWord.split("").reverse().join("");
+	console.log(translatedWord);
+	await browser.close();
+	return translatedWord;
+};
+
+arabicToHebrew("مرحبتين");
+hebrewToArabic("שלום");
+hebrewToArabicInHebrewText("שלום");
+hebrewToArabicInEnglishText("שלום");
